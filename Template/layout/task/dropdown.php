42c8cdf87f2ff3e8d6557418464c109420e24fc4
David Morlitz <david@morlitz.com>
2018-09-28 08:20:58 -0700

    Added last comment to drop down menu - with on/off switch

--- Template/layout/task/dropdown.php
+++ Template/layout/task/dropdown.php
@@ -1,7 +1,4 @@
 <?php
-     // PUSH INTERVAL 1
-?>
-<?php
              $CardPushDate_interval_1 = $this->task->projectMetadataModel->get($task['project_id'], "CardPushDate_interval_1");
              $CardPushDate_interval_2 = $this->task->projectMetadataModel->get($task['project_id'], "CardPushDate_interval_2");
              $CardPushDate_interval_3 = $this->task->projectMetadataModel->get($task['project_id'], "CardPushDate_interval_3");
@@ -20,6 +17,34 @@
 
              $CardPushDate_show_comment_in_dropdown = $this->task->projectMetadataModel->get($task['project_id'], "CardPushDate_show_comment_in_dropdown");
 ?>
+
+<?php
+/************************************************************************************************************************/
+/** Display the last comment on the menu - if requested *****************************************************************/
+/************************************************************************************************************************/
+?>
+
+<?php //Display the last comment on the card - if requested by settings ?>
+<?php if ($this->user->hasProjectAccess('TaskModificationController', 'edit', $task['project_id'])): ?>
+         <?php
+             if ($CardPushDate_show_comment_in_dropdown == "1") {
+                 $comments = $this->task->commentModel->getAll($task['id'], 'ASC');
+                 foreach ($comments as $comment):
+                     $display_comment = date("m/d/Y", $comment['date_creation']) . ': ' . $comment['comment']; //Keep overwriting the displayed comment until we reach the last one
+                 endforeach;
+                 if ($display_comment != "") {
+                     echo "<li>" . $display_comment;
+                 }
+             }
+         ?>
+<?php endif; ?>
+
+<?php
+/************************************************************************************************************************/
+/** Push interval 1 *****************************************************************************************************/
+/************************************************************************************************************************/
+?>
+
 <?php if ($this->user->hasProjectAccess('TaskModificationController', 'edit', $task['project_id'])) {
 		$CardPushDate_interval_1 = $this->task->projectMetadataModel->get($task['project_id'] , 'CardPushDate_interval_1');
                 if ($CardPushDate_interval_1 == "") {
@@ -158,18 +183,3 @@
 		</li>
          <?php endif ?>
 <?php endif ?>
-
-<?php //Display the last comment on the card - if requested by settings ?>
-<?php if ($this->user->hasProjectAccess('TaskModificationController', 'edit', $task['project_id'])): ?>
-         <?php
-             if ($CardPushDate_show_comment_in_dropdown == "1") {
-                 $comments = $this->task->commentModel->getAll($task['id'], 'ASC');
-                 foreach ($comments as $comment):
-                     $display_comment = date("m/d/Y", $comment['date_creation']) . ': ' . $comment['comment']; //Keep overwriting the displayed comment until we reach the last one
-                 endforeach;
-                 if ($display_comment != "") {
-                     echo "<li>" . $display_comment;
-                 }
-             }
-         ?>
-<?php endif; ?>

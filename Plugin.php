20294fb4e45cd7db4ca3517d6aed7096e2dc9701
David Morlitz <david@morlitz.com>
2019-05-30 07:57:41 -0400

    Minor error correction

--- Plugin.php
+++ Plugin.php
@@ -8,7 +8,6 @@ class Plugin extends Base
 {
     public function initialize()
     {
-        $this->template->hook->attach('template:board:private:task:after-title', 'CardPushDate:layout/footer');
         $this->template->hook->attach('template:search:task:footer', 'CardPushDate:layout/footer');
         //$this->template->hook->attach('template:dashboard:task:footer', 'CardPushDate:layout/footer');
         //$this->template->hook->attach('template:board:task:footer', 'CardPushDate:layout/footer');

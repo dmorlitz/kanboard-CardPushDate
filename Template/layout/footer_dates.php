c13f8c2d8cd6c675886aa953af757e18f76c3366
David Morlitz <david@morlitz.com>
2026-02-05 06:57:35 -0500

    Removed debugging message

--- Template/layout/footer_dates.php
+++ Template/layout/footer_dates.php
@@ -1,4 +1,3 @@
-Hello
 <?php if ($this->user->hasProjectAccess('TaskModificationController', 'edit', $task['project_id'])): ?>
          <?php
              $CardPushDate_interval_1 = $this->task->projectMetadataModel->get($task['project_id'], "CardPushDate_interval_1");

84ac75ff7bcfcf58ddeffff8da9ca7878efacb9d
David Morlitz <david@morlitz.com>
2026-04-05 09:31:29 -0400

    Removed duplicate tag display

--- Template/task_list/task_details.php
+++ Template/task_list/task_details.php
@@ -243,13 +243,6 @@
                 }
             ?>
 
-            <?php //DMM: Showing tags in the compact view ?>
-            <?php foreach ($task['tags'] as $tag): ?>
-                <span class="table-list-category task-list-tag <?= $tag['color_id'] ? "color-{$tag['color_id']}" : '' ?>">
-                    <?= $this->text->e($tag['name']) ?>
-                </span>
-            <?php endforeach ?>
-
         <?php endif; ?>
     <?php else: ?>
         <div class="task-board-expanded">
